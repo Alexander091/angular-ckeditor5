4fd2e295a710223ab8ec9dbe4579a9ec9a94147f
Alekseev Aleksandr <alexander.alekseev091@gmail.com>
2020-11-09 23:23:44 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,30 @@
-import { Component, OnInit } from "@angular/core";
-import * as ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import {Component, OnInit} from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
-  name = "Angular";
-  editor = ClassicEditor;
+  title = 'angular-ckeditor5';
+  public Editor = ClassicEditor;
   config = {};
 
-  ngOnInit() {
-    // this.config = {
-    //   ignoreEmptyParagraph: true,
-    //   extraPlugins: [
-    //     editor => {
-    //       return this.uploadAdapterPlugin(editor);
-    //     }
-    //   ]
-    // };
+  ngOnInit(): void {
+    this.config = {
+      ignoreEmptyParagraph: true,
+      extraPlugins: [
+        editor => {
+          return this.uploadAdapterPlugin(editor);
+        }
+      ]
+    };
   }
 
-  // uploadAdapterPlugin(editor) {
-  //   editor.plugins.get('FileRepository').createUploadAdapter = loader => {
-  //     console.log(`Do something`)
-  //   };
-  // }
+  uploadAdapterPlugin(editor): void {
+    editor.plugins.get('FileRepository').createUploadAdapter = loader => {
+      console.log(`Do something`);
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { NgModule } from '@angular/core';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
+import { AppComponent } from './app.component';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, CKEditorModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CKEditorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
